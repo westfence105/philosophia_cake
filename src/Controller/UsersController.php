156623be0c0faf996160f4d464890140ad264c5a
Togo Nishigaki <westfence95@gmail.com>
2016-10-12 11:25:24 +0900

    Implement login action

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -21,7 +21,16 @@ class UsersController extends AppController
 
     public function login()
     {
-
+        if( $this->request->is('post') ){
+            $user = $this->Auth->identify();
+            if( $user ){
+                $this->Auth->setUser($user);
+                return $this->redirect( $this->Auth->redirectUrl() );
+            }
+            else {
+                $this->Flash->error(__('Username or Password is incorrect.') );
+            }
+        }
     }
 }
 
