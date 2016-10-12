e2895c671472bd4389ac8863b8297a1bc33f50e7
Togo Nishigaki <westfence95@gmail.com>
2016-10-12 14:25:43 +0900

    Implement register action

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -34,7 +34,24 @@ class UsersController extends AppController
     }
 
     public function register(){
-        
+        if ($this->request->is('post') ){
+            $user = $this->Users->newEntity( $this->request->data );
+    
+            if( $user->errors() ){
+                $this->Flash->error( __('Form data has invalid content.') );
+            }
+            else if( $this->Users->save($user) ){
+                $this->Flash->success( __x('registration completed','Success') );
+            }
+            else{
+                $this->Flash->error( __('Failed to add to database.') );
+            }
+
+            $this->set( 'entities', $user );
+        }
+        else {
+            $this->set( 'entities', null );
+        }
     }
 }
 
--- src/Template/Users/register.ctp
+++ src/Template/Users/register.ctp
@@ -1,7 +1,7 @@
 <h2><?= __('Register') ?></h2>
 <?php
-	echo $this->Form->create('Post'), "\n";
-	echo $this->Form->input( 'username',[ 'label' => __('Username'), 'style' => 'width: 24em;' ] ), "\n";
-	echo $this->Form->input( 'password',[ 'label' => __('Password'), 'style' => 'width: 24em;' ] ), "\n";
+	echo $this->Form->create( $entities, [ 'type' => 'Post' ]), "\n";
+	echo $this->Form->input( 'username', [ 'type' => 'text',     'label' => __('Username'), 'required' => true, 'style' => 'width: 24em;' ] ), "\n";
+	echo $this->Form->input( 'password', [ 'type' => 'password', 'label' => __('Password'), 'required' => true, 'style' => 'width: 24em;' ] ), "\n";
 	echo $this->Form->submit(__x('text of register form submit button','Register') ), "\n";
 ?>
