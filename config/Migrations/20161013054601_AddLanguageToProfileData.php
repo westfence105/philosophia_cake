ca25b01a2edeabbc1b90bae83b0a30ec78f6e30b
Togo Nishigaki <westfence95@gmail.com>
2016-10-13 15:34:53 +0900

    Add language column to database

---
+++ config/Migrations/20161013054601_AddLanguageToProfileData.php
@@ -0,0 +1,23 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddLanguageToProfileData extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('profile_data');
+        $table->addColumn('language', 'string', [
+            'default' => '',
+            'limit' => 3,
+            'null' => false
+        ]);
+        $table->update();
+    }
+}
---
+++ config/Migrations/20161013060713_AddLanguageToUser.php
@@ -0,0 +1,23 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddLanguageToUser extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('language', 'string', [
+            'default' => null,
+            'limit' => 3,
+            'null' => true,
+        ]);
+        $table->update();
+    }
+}
